5e8c5a8d1c6c79b1ba292ab2afadfa7469c65aeb
XaXaToXaXa <XaXaToXaXa@gmail.com>
2023-01-14 20:05:15 +0300

    First task

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -3,3 +3,18 @@
 
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+Console.WriteLine("Введите первое число ");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine("Введите второе число ");
+int rate = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine($"Число {num} возведенное в степень {rate} равно {task(num, rate)}");
+
+int task(int number, int degree)
+{
+    int result = number;
+   for (int i = 1; i < degree; i++)
+   {
+   result = result * number;
+   }
+   return result;
+}
